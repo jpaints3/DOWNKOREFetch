c759aef430fce2e8698f958ece2def8b3b065b57
Josiah <jopaintsil@gmail.com>
2021-05-13 21:37:00 -0400

    Create working test server

--- server.js
+++ server.js
@@ -1,20 +1,15 @@
-var express = require("express")
-var logger = require("morgan")
+const fs = require('fs');
+const http = require('http');
 
-var app = express()
+const hostname = '127.0.0.1';
+const port = 3000;
 
-app.get('/', function(request, response){
-    response.render('index.html')
-})
-
-app.set('view engine', 'html')
-
-app.use (express.static('views'))
-app.set ('views', __dirname + '/views')
-app.use(logger('dev'))
-
-var port = process.env.PORT || 8080
-
-app.listen(port, function(){
-    console.log (`App running`)
-})
+http.createServer(function (req, res) {
+    fs.readFile(__dirname + "/views/index.html", function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html ; charset=UTF-8'});
+      res.write(data);
+      return res.end();
+    });
+  }).listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
