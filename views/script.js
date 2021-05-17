9087623597053fb80128d25168884cb2022fbb8d
Josiah <jopaintsil@gmail.com>
2021-05-17 08:05:48 -0400

    Enable prettier

--- views/script.js
+++ views/script.js
@@ -2,13 +2,13 @@ const firebase = require("firebase");
 // Required for side-effects
 require("firebase/firestore");
 
-async function sendFiles(URL, DATA){
+async function sendFiles(URL, DATA) {
     await fetch(URL, {
         'method': 'POST',
         'headers': {
             'Content-Type': 'application/json'
         },
-        'body':JSON.stringify(DATA)
+        'body': JSON.stringify(DATA)
     })
     return response.json();
 }
