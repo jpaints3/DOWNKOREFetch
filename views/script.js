cae74741ebeaf98d73222f111d0fc3bf679074a4
Josiah <jopaintsil@gmail.com>
2021-05-25 00:33:26 -0400

    Create locally stored token page and read it

--- views/script.js
+++ views/script.js
@@ -1,17 +1,47 @@
-const firebase = require("firebase");
-// Required for side-effects
-require("firebase/firestore");
+const firebase = require('firebase/app');
+require('firebase/firestore');
 
-async function sendFiles(URL, DATA) {
-    await fetch(URL, {
-        'method': 'POST',
-        'headers': {
-            'Content-Type': 'application/json'
-        },
-        'body': JSON.stringify(DATA)
-    })
-    return response.json();
-}
 
-document.getElementById('uploadBtn').onclick() = sendFiles(URL, DATA);
+let tokenINFO = '';
+APIKey = '';
+AUTHDomain = '';
+PROJECTId = '';
+
+const fileUrl = window.location.href + 'tokensPlusAuth.txt'; // provide file location
+
+fetch(fileUrl)
+   .then( r => r.text() )
+   .then( t => {
+        console.log(t.split(/\n/));
+        let tokenINFO = t.split(/\n/);
+        APIKey = tokenINFO[1];
+        AUTHDomain = tokenINFO[3];
+        PROJECTId = tokenINFO[5];
+
+        /*/ Initialize Cloud Firestore through Firebase
+        firebase.initializeApp({
+            apiKey: APIKey,
+            authDomain: AUTHDomain,
+            projectId: PROJECTId
+        });
+
+        var db = firebase.firestore();
+
+        async function sendFiles(URL, DATA) {
+            await fetch(URL, {
+                'method': 'POST',
+                'headers': {
+                    'Content-Type': 'application/json'
+                },
+                'body': JSON.stringify(DATA)
+            })
+            return response.json();
+        }
+
+        document.getElementById('uploadBtn').onclick() = sendFiles(URL, DATA); */
+    });
+
+
+
+
 
