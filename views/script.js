19e69306f6fde7ed07793e98850f8928fdc1a63a
Josiah <jopaintsil@gmail.com>
2021-05-27 02:07:51 -0400

    Add specifics to template for uploading user video files

--- views/script.js
+++ views/script.js
@@ -6,9 +6,14 @@ const fileUrl = window.location.href + 'tokensPlusAuth.txt'; // provide file loc
 fetch(fileUrl)
     .then(r => r.text())
     .then(t => {
+
+        //<------------- Initialize Firestore Storage ------------->
+
+        // access the text file with sensitive information
         console.log(t.split(/\n/));
         let tokenINFO = t.split(/\n/);
 
+        // access firebase storage with this information
         let firebaseConfig = {
             apiKey: tokenINFO[2],
             authDomain: tokenINFO[4],
@@ -18,19 +23,27 @@ fetch(fileUrl)
 
         firebase.initializeApp(firebaseConfig);
         var storage = firebase.storage();
+
+        // create location references
         var storageRef = storage.ref();
         var videosRef = storageRef.child('videos');
 
-        let file = null;
-        let fileName = '';
-        let fileRef = videosRef.child(fileName);
 
-        fileRef.put(file).then(() => {
-            console.log('Uploaded a file!');
-        });
+        //<------------- Place User Files in Storage ------------->
 
+        function sendFiles(videosRef, dancerName, inputFiles){
+            let fileName = dancerName;
+            let fileRef = videosRef.child(fileName);
 
-        /*document.getElementById('uploadBtn').onclick() = sendFiles(URL, DATA);  */
+            fileRef.put(inputFiles).then(() => {
+                console.log('Uploaded a file!');
+            });
+        }
+
+        let dancerName = document.getElementById('dancerName').value;
+        let inputFiles = document.getElementById('inputFiles').files;
+        document.getElementById('uploadBtn').onclick() = sendFiles(videosRef, dancerName, inputFiles);
+
     });
 
 
